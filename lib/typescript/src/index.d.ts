8918ecef9771fe1c2bdb63c1d73606f28ba24fa5
Andrei Datcu <datcuandrei@gmail.com>
2020-06-27 01:33:18 +0300

    gitpkg

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Navigators
+ */
+export { default as createBottomTabNavigator } from './navigators/createBottomTabNavigator';
+/**
+ * Views
+ */
+export { default as BottomTabView } from './views/BottomTabView';
+/**
+ * Types
+ */
+export type { BottomTabNavigationOptions, BottomTabNavigationProp, BottomTabScreenProps, BottomTabBarProps, BottomTabBarOptions, } from './types';
